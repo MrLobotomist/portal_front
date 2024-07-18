fd27cb08fceba617132545cdfe4eb45e4691830c
MrLobotomist <39457028+MrLobotomist@users.noreply.github.com>
2024-07-18 09:36:00 +0500

    Update profile.tsx

--- src/pages/profile/ui/profile.tsx
+++ src/pages/profile/ui/profile.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/widgets/navbar/ui/navbar.tsx';
 import { Footer } from '@/widgets/footer/ui/Footer.tsx';
 import styles from '@/shared/styles/main.module.sass';
 import UserProfile from '@/widgets/userProfile/ui/userProfile.tsx';
+import grid from '@/shared/styles/grid.module.sass';
 
 export const Profile = () => {
   return (
